8ac0657071ce939aa6f997cd8c8c2516eab3ba42
Prakash <prakash.hartron@gmail.com>
2019-04-19 12:35:02 +0530

    Chalk, axios and figlet packages added to style and call api

--- src/commands/quote.ts
+++ src/commands/quote.ts
@@ -1,6 +1,6 @@
 import {Command, flags} from '@oclif/command'
 import axios from 'axios'
-
+var figlet = require('figlet');
 const chalk = require('chalk');
 
 export default class Quote extends Command {
@@ -10,6 +10,14 @@ export default class Quote extends Command {
     const {args} = this.parse(Quote)
     const {data: response} = await axios.get(`https://got-quotes.herokuapp.com/quotes?char=${args.character}`)
     var quote = response;
-    this.log('\n' + chalk.blue.underline.bold(quote.character)+ ' : ' +chalk.blue(quote.quote) + '\n');
+    figlet('Game of thrones', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      console.log(data);
+    });
+    this.log('\n ' + chalk.blue.underline.bold(quote.character)+ ' : ' +chalk.blue(quote.quote) + '\n');
   }
 }
